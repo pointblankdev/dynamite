5e4b845ab16de595d14af7dc27f7534ce1c715b2
Ross Ragsdale <ross.ragsdale@gmail.com>
2021-07-20 16:55:09 -0500

    :children_crossing: Add default export.

--- src/Dynamite.ts
+++ src/Dynamite.ts
@@ -1,4 +1,5 @@
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { Table, Entity } from 'dynamodb-toolbox'
 
+export { Table, Entity, DocumentClient }
 export default { Table, Entity, DocumentClient }
